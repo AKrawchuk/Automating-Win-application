5a56ac8079bbfc6b9751cd1b53cd4d11ec1249f7
krawchuk25@gmail.com <semen1semen1>
2017-11-06 00:16:26 +0200

    Task 18: add tests for creating/removing groups using AutoItX

---
+++ Addressbook_tests_autoIt/Addressbook_tests_autoIt/Tests/GroupDeleteTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Addressbook_tests_autoIt
+{
+    public class GroupDeleteTests : TestBase
+    {
+        [Test]
+        public void TestGroupDelete()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            app.Groups.Delete(0);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.RemoveAt(0);
+
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
---
+++ Addressbook_tests_autoIt/Addressbook_tests_autoIt/Tests/TestBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Addressbook_tests_autoIt
+{
+    public class TestBase
+    {
+        public ApplicationManager app;
+
+        [SetUp]//[TestFixtureSetUp]
+        public void initApplication()
+        {
+            app = new ApplicationManager();
+        }
+
+        [TearDown]//[TestFixtureTearDown]
+        public void stopApplication()
+        {
+            app.Stop();
+        }
+    }
+}
